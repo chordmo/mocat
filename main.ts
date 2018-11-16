3f0c66d07b4611f606334f8451b31648a823fcbb
chordmo <chordmo@gmail.com>
2018-11-17 00:50:08 +0800

    init

--- main.ts
+++ main.ts
@@ -26,7 +26,7 @@ function createWindow() {
     win.loadURL('http://localhost:4200');
   } else {
     win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/mocat/index.html'),
+      pathname: path.join(__dirname, 'dist/index.html'),
       protocol: 'file:',
       slashes: true
     }));
